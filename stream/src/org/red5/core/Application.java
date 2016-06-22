01f65fb2b2d6423795069e6b05f4b130a86ee490
jacobplaster <j.plaster@2013.hull.ac.uk>
2016-06-22 18:18:39 +0100

    created successful remoting connection with the red5 service

--- stream/src/org/red5/core/Application.java
+++ stream/src/org/red5/core/Application.java
@@ -10,10 +10,13 @@ package org.red5.core;
  *  - libs/slf4j-api
  */
 
+import java.io.IOException;
+
 import org.red5.logging.Red5LoggerFactory;
 import org.red5.server.adapter.MultiThreadedApplicationAdapter;
 import org.red5.server.api.IConnection;
 import org.red5.server.api.scope.IScope;
+import org.red5.server.net.remoting.RemotingClient;
 import org.slf4j.Logger;
 
 /**
@@ -30,10 +33,37 @@ public class Application extends MultiThreadedApplicationAdapter {
     @Override
 	public boolean connect(IConnection conn, IScope scope, Object[] params) {
     	log.info("[STREAM] New connection attempt from " + conn.getRemoteAddress());
-    	// if is allowed to publish
-    	return true;
-    	// else
-    	//return false;
+    	
+    	try 
+    	{
+    		// was working with amfphp 2.2 but was returning long double
+    		// amfphp 1.6 is compatable with red5 but requires php 5.4 (slow af) and mamp doesnt support
+    		// amf_zend is an alternative but effort to setup and no red5 documentation 
+    		//RemotingClient client = new RemotingClient("http://localhost/learnx-service/flash-services/index.php");
+    		RemotingClient client = new RemotingClient("http://localhost/learnx-service/flash-services/gateway.php");
+    		
+    		Object[] args = new Object[]{"admin","admin"};
+    		System.out.println("\n\n-- Connect called: --\n\n");
+    		
+    		Object result = client.invokeMethod("red5service.login", args);
+    		
+    		// AMFPHP 2.2.2
+    		//Object result = client.invokeMethod("service.red5service.login", args);
+    		
+    		System.out.println("[AMFPHP response] "+ result);
+
+    		if(!result.equals(true)) 
+			{
+    			log.info("[STREAM] User ("+ conn.getRemoteAddress() +") does not have access.");
+				return false;  
+			}
+		} catch (Exception e) 
+    	{
+			log.info("[STREAM] Unable to connect \""+ conn.getRemoteAddress() +"\" to flash services");
+    		return false;
+    	}
+    	log.info("[STREAM] \""+ conn.getRemoteAddress() +"\" connected to flash services successfully");
+    	return true;   
 	}
 
 	/** {@inheritDoc} */
